9d295bad64bd8a06d1d7395a0d0880c62565a682
nisankas <satchit.nisanka@hp.com>
2015-09-10 22:05:00 -0700

    ops-cli Repo transition.
    Repo transition (story# 2486)
    ---------------
    1. Included authors to AUTHORS files.
    2. Modified NEWS, Changelog and TODO file to remove old Quagga stuff.
    3. Remove Quagga banner in vtysh.
    4. Changed License header of code files.
    
    Change-Id: I4931de37b9fb2a4c58cd508a52ffed8d2a117085
    Signed-off-by: nisankas <satchit.nisanka@hp.com>

--- vtysh/vtysh_config.c
+++ vtysh/vtysh_config.c
@@ -1,5 +1,6 @@
 /* Configuration generator.
    Copyright (C) 2000 Kunihiro Ishiguro
+   Copyright (C) 2015 Hewlett Packard Enterprise Development LP
 
 This file is part of GNU Zebra.
 
--- vtysh/vtysh_user.c
+++ vtysh/vtysh_user.c
@@ -1,5 +1,6 @@
 /* User authentication for vtysh.
  * Copyright (C) 2000 Kunihiro Ishiguro
+ * Copyright (C) 2015 Hewlett Packard Enterprise Development LP
  *
  * This file is part of GNU Zebra.
  *
