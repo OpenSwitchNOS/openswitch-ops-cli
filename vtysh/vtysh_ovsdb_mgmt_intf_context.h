7a4393d83e6d6bbf97caf025e1933d51fb3222bc
vkez <ezhilan.vk@hpe.com>
2015-09-15 01:08:06 -0700

    Management interface source file CLI cleanup.
    Change-Id: I3930521adedf0b64f3139026d96f84288fbe47df
    Signed-off-by: vkez <ezhilan.vk@hpe.com>

--- vtysh/vtysh_ovsdb_mgmt_intf_context.h
+++ vtysh/vtysh_ovsdb_mgmt_intf_context.h
@@ -1,26 +1,26 @@
-/*
- Copyright (C) 2015 Hewlett Packard Enterprise Development LP
- All Rights Reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License"); you may
- not use this file except in compliance with the License. You may obtain
- a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- License for the specific language governing permissions and limitations
- under the License.
-*/
-/****************************************************************************
- * @ingroup cli
+/* Management interface client callback resigitration header file.
+ * Copyright (C) 2015 Hewlett Packard Enterprise Development LP.
  *
- * @file vtysh_ovsdb_mgmt_intf_context.h
- * Source for registering client callback with management interface context.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
  *
- ***************************************************************************/
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * File: vtysh_ovsdb_mgmt_intf_context.h
+ *
+ * Purpose:  To add declarations required for vtysh_ovsdb_mgmt_intf_context.c
+ *
+ */
 
 #ifndef VTYSH_OVSDB_MGMT_INTF_CONTEXT_H
 #define VTYSH_OVSDB_MGMT_INTF_CONTEXT_H
