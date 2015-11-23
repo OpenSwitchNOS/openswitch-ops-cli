97a690c9917935739fb572850f5809b4bc12f121
Celestian K. S <celestian.k-sebastian@hpe.com>
2015-11-23 00:12:08 -0800

    Subinterface & loopback interface demo comments and CT scripts
    Change-Id: I095566ea111a9953e797cc84adb1da58ea6d1997
    Signed-off-by: Celestian K. S <celestian.k-sebastian@hpe.com>

---
+++ vtysh/tests/test_vtysh_ct_new_sub_intf_cli.py
@@ -0,0 +1,182 @@
+import pytest
+import re
+from opstestfw import *
+from opstestfw.switch.CLI import *
+from opstestfw.switch import *
+
+# Topology definition
+topoDict = {"topoExecution": 1000,
+            "topoTarget": "dut01",
+            "topoDevices": "dut01 wrkston01",
+            "topoLinks": "lnk01:dut01:wrkston01",
+            "topoFilters": "dut01:system-category:switch,\
+                            wrkston01:system-category:workstation"}
+
+
+def subintf_cli(**kwargs):
+
+    device1 = kwargs.get('device1', None)
+    device2 = kwargs.get('device2', None)
+
+# creating asub interface and verifying in port and interface table
+    retStruct = InterfaceEnable(deviceObj=device1, enable=True,
+                                interface="4.2")
+
+    if retStruct.returnCode() != 0:
+        LogOutput('error', "Failed to enable interface")
+        assert(False)
+
+    LogOutput('info', "### Verify the interface is created with'\
+              same name for L3 port ###")
+    devIntReturn = device1.DeviceInteract(command=
+                                          "ovs-vsctl get interface 4.2 name")
+    retCode = devIntReturn.get('buffer')
+    assert "4.2" in retCode, "Failed to retrieve ovs-vsctl command"
+    LogOutput('info', "### interface 4.2 created successfully ###")
+
+    devIntReturn = device1.DeviceInteract(command=
+                                          "ovs-vsctl get port 4.2 name")
+    retCode = devIntReturn.get('buffer')
+    assert "4.2"in retCode, "Failed to retrieve ovs-vsctl command"
+    LogOutput('info', "### interface 4.2 created successfully ###")
+
+# configuring the ip address and verifying ip address in port table
+    retStruct = InterfaceIpConfig(deviceObj=device1,
+                                  interface="4.3",
+                                  addr="192.168.1.2", mask=24, config=True)
+
+    if retStruct.returnCode() != 0:
+        LogOutput('error',
+                  "### Failed to configure interface IPV4 address ###")
+        assert(False)
+
+    devIntReturn = device1.DeviceInteract(command=
+                                          "ovs-vsctl get port 4.3 ip4_address")
+    retCode = devIntReturn.get('buffer')
+    assert '192.168.1.2/24' in retCode, \
+           'Test to verify sub-interface configuration clis - FAILED!'
+
+# verifing in assigning Invalid Ip address
+    retStruct = InterfaceIpConfig(deviceObj=device1,
+                                  interface="4.5",
+                                  addr="0.0.0.0", mask=24, config=True)
+    retCode = retStruct.buffer()
+    if 'Invalid IP address' in retCode:
+        LogOutput('info', "Invalid IPV4 address")
+
+# verifing in assigning Invalid Ip address
+    retStruct = InterfaceIpConfig(deviceObj=device1,
+                                  interface="4.4",
+                                  addr="255.255.255.255", mask=24, config=True)
+    retCode = retStruct.buffer()
+    if 'Invalid IP address' in retCode:
+        LogOutput('info', "Invalid IPV4 address")
+
+# configuring the ipv6 address and verifying in port table
+    retStruct = InterfaceIpConfig(deviceObj=device1,
+                                  interface="4.4",
+                                  addr="10:10::10:10", ipv6flag=True,
+                                  mask=24, config=True)
+    devIntReturn = device1.DeviceInteract(command=
+                                          "ovs-vsctl get port 4.4 ip6_address")
+    retCode = devIntReturn.get('buffer')
+    assert '10:10::10:10/24' in retCode, \
+           'Test to verify sub-interface configuration clis - FAILED!'
+
+# verifing same vlan for different subinterface
+    retStruct = Dot1qEncapsulation(deviceObj=device1, subInterface="4.3",
+                                   dot1q=True, vlan=100)
+    if retStruct.returnCode() != 0:
+        LogOutput('error', "Failed to enable interface")
+        assert(False)
+
+    retStruct = Dot1qEncapsulation(deviceObj=device1, subInterface="4.8",
+                                   dot1q=True, vlan=100)
+    retCode = retStruct.buffer()
+    if 'Encapsulation VLAN is already configured on interface 4.3.' in retCode:
+        LogOutput('info', "Dot1Q encapsulation already assigned !.")
+
+    retStruct = Dot1qEncapsulation(deviceObj=device1, subInterface="4.8",
+                                   dot1q=False, vlan=100)
+    retCode = retStruct.buffer()
+    if 'Encapsulation VLAN is already configured on interface 4.3.' in retCode:
+        LogOutput('info', "Dot1Q encapsulation already assigned !.")
+
+    retStruct = InterfaceIpConfig(deviceObj=device1,
+                                  interface="4.8",
+                                  addr="192.168.1.2", mask=24, config=True)
+
+    retCode = retStruct.buffer()
+    if 'Duplicate IP Address.' in retCode:
+        LogOutput('info', "Ip already assigned !.")
+
+    retStruct = Dot1qEncapsulation(deviceObj=device1, subInterface="4.8",
+                                   dot1q=False, vlan=100)
+    devIntReturn = device1.DeviceInteract(command="do show running-config")
+    retCode = retStruct.buffer()
+    if 'encapsulation dot1Q 100' in retCode:
+        LogOutput('info', "encapsulation dot1Q still exist-Fail!")
+
+    retStruct = InterfaceEnable(deviceObj=device1, enable=True,
+                                interface="4")
+    retStruct = InterfaceIpConfig(deviceObj=device1,
+                                  interface="4",
+                                  routing=True, config=False)
+
+    devIntReturn = device1.DeviceInteract(command="vtysh")
+    devIntReturn = device1.DeviceInteract(command="conf t")
+
+    devIntReturn = device1.DeviceInteract(command="int 12.24545435612431562")
+    retCode = devIntReturn.get('buffer')
+    if "Invalid input"in retCode:
+        LogOutput('info', "Canot creat the interface ")
+
+    devIntReturn = device1.DeviceInteract(command="no int 12.2")
+    retCode = devIntReturn.get('buffer')
+    if "Interface does not exist"in retCode:
+        LogOutput('info', "no interface exist failed!")
+
+    devIntReturn = device1.DeviceInteract(command="int 4.9")
+    retCode = devIntReturn.get('buffer')
+    if 'Parent interface is not L3' in retCode:
+        LogOutput('info', "canot create subinterface  in L2 interface")
+
+    devIntReturn = device1.DeviceInteract(command="no int 4.2")
+    devIntReturn = device1.DeviceInteract(command="no int 4.2")
+    retCode = devIntReturn.get('buffer')
+    if "Interface does not exist"in retCode:
+        LogOutput('info', "sub interface already deleted !.")
+
+    devIntReturn = device1.DeviceInteract(command="int 100.1025")
+    retCode = devIntReturn.get('buffer')
+    if "Parent interface does not exis"in retCode:
+        LogOutput('info', "parent interface is not present!.")
+
+    devIntReturn = device1.DeviceInteract(command="exit")
+    devIntReturn = device1.DeviceInteract(command="exit")
+
+    devIntReturn = device1.DeviceInteract(command=
+                                          "ovs-vsctl get interface 4.2 name")
+    retCode = devIntReturn.get('buffer').splitlines()
+    if len(retCode) == 3:
+        retCode = retCode[1]
+    if "no row \"4.2\" in table Interface" in retCode:
+        LogOutput('info', "### interface 4.2 deleted ! ###")
+
+
+class Test_subintf_cli:
+
+    def setup_class(cls):
+        # Test object will parse command line and formulate the env
+        Test_subintf_cli.testObj = testEnviron(topoDict=topoDict)
+        # Get topology object
+        Test_subintf_cli.topoObj = Test_subintf_cli.testObj.topoObjGet()
+
+    def teardown_class(cls):
+        Test_subintf_cli.topoObj.terminate_nodes()
+
+    def test_subintf_cli(self):
+        LogOutput('info', "**configuring**")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        wrkston1Obj = self.topoObj.deviceObjGet(device="wrkston01")
+        retValue = subintf_cli(device1=dut01Obj, device2=wrkston1Obj)
