8645633d60780f59624756acd1e903744302eb56
Vinay K <vinay.k@hpe.com>
2015-11-29 22:49:22 -0800

    Sftp Server CLI daemon.
    Change-Id: I191298d6289f085d510da2c7f2a622a0b1d84ba8
    Signed-off-by: Vinay K <vinay.k@hpe.com>

---
+++ vtysh/sftp_vty.h
@@ -0,0 +1,66 @@
+/* SFTP CLI commands header file
+ *
+ * Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * File: sftp_vty.h
+ *
+ * Purpose:  To add declarations required for sftp_vty.c
+ */
+
+#ifndef _SFTP_VTY_H
+#define _SFTP_VTY_H
+
+#include <sys/un.h>
+#include <setjmp.h>
+#include <sys/wait.h>
+#include <pwd.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include <lib/version.h>
+#include <unistd.h>
+#include "getopt.h"
+#include "command.h"
+#include "memory.h"
+#include "vtysh/vtysh.h"
+#include "vtysh/vtysh_user.h"
+#include "vswitch-idl.h"
+#include "ovsdb-idl.h"
+#include "sftp_vty.h"
+#include "smap.h"
+#include "openvswitch/vlog.h"
+#include "openswitch-idl.h"
+#include "vtysh/vtysh_ovsdb_if.h"
+#include "vtysh/vtysh_ovsdb_config.h"
+#include <arpa/inet.h>
+#include <string.h>
+
+/* OVSDB key-value pair. */
+#define SFTP_SERVER_CONFIG      "sftp_server_enable"
+
+#define SFTP_STR \
+"SSH File Transfer Protocol\n"
+#define SFTP_SERVER \
+"SFTP server configuration(Default: Disable)\n"
+#define ENABLE_STR \
+"Enable SFTP server\n"
+
+void
+sftp_vty_init(void);
+
+#endif /* SFTP_VTY_H */
