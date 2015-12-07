11a522dce028f0deb1ceb7ee4be273eebe9d8dc3
Vinay K <vinay.k@hpe.com>
2015-12-07 02:14:58 -0800

    SFTP client support
    Change-Id: Ifc8d3f8b6594b073c0f23954421ac7e8757503ff
    Signed-off-by: Vinay K <vinay.k@hpe.com>

--- vtysh/sftp_vty.h
+++ vtysh/sftp_vty.h
@@ -50,6 +50,10 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+/* Max lengths. */
+#define MAX_USERNAME_LEN        256
+#define MAX_HOSTNAME_LEN        256
+
 /* OVSDB key-value pair. */
 #define SFTP_SERVER_CONFIG      "sftp_server_enable"
 
@@ -59,6 +63,35 @@
 "SFTP server configuration(Default: Disable)\n"
 #define ENABLE_STR \
 "Enable SFTP server\n"
+
+#define SFTP_STR \
+"SSH File Transfer Protocol\n"
+#define SFTP_CLIENT_STR \
+"Copy data from an SFTP server\n"
+#define SRC_FILENAME \
+"Specify source filename for the SFTP transfer\n"
+#define USERNAME_STR \
+"Specify the username string (Max Length 256) on the remote system information\n"
+#define HOST_IPv4 \
+"Specify the host IP of the remote system (IPv4)\n"
+#define HOST_IPv6 \
+"Specify the host IP of the remote system (IPv6)\n"
+#define HOSTNAME_STR \
+"Specify the hostname string (Max Length 256) of the remote system\n"
+#define DST_FILENAME \
+"Specify destination filename (Default path: '/var/local/')\n"
+#define DEFAULT_DST \
+"/var/local/"
+
+/* Store the details for SFTP client action. */
+typedef struct sftpClient_t {
+    char *userName;     /* Stores the username of remote system. */
+    char *hostName;     /* Stores the hostname of remote system. */
+    char *srcFile;      /* Stores the source filename for SFTP transfer. */
+    char *dstFile;      /* Stores the destination path. */
+    bool isInteractive; /* Flag to indicate SFTP client is
+                         * interactive or non-interactive. */
+} sftpClient;
 
 void
 sftp_vty_init(void);
