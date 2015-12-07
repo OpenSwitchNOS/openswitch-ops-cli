ed25467573f8faed6a730ecdbd97db7354221d8e
Vinay K <vinay.k@hpe.com>
2015-12-07 02:14:58 -0800

    SFTP client support
    Change-Id: Ifc8d3f8b6594b073c0f23954421ac7e8757503ff
    Signed-off-by: Vinay K <vinay.k@hpe.com>

--- vtysh/sftp_vty.c
+++ vtysh/sftp_vty.c
@@ -25,6 +25,7 @@
 
 static int show_sftp_server (void);
 static int sftp_server_enable_disable (bool);
+static int sftp_client_copy (sftpClient *sc);
 
 extern struct ovsdb_idl *idl;
 VLOG_DEFINE_THIS_MODULE(vtysh_sftp_cli);
@@ -127,6 +128,75 @@ sftp_server_enable_disable (bool enable)
     }
 }
 
+/*-----------------------------------------------------------------------------
+| Function : sftp_client_copy
+| Responsibility : To handle SFTP client copy operation.
+| Parameters :
+|     sftpClient *sc: Pointer to hold all the information needed to perform
+|                     SFTP copy.
+| Return : On success returns CMD_SUCCESS,
+|          On failure returns CMD_WARNING
+-----------------------------------------------------------------------------*/
+static int
+sftp_client_copy (sftpClient *sc)
+{
+    int ret = CMD_SUCCESS;
+    int len = 0, argc = 2;
+    int cmd_len = 0;
+    char *dest = NULL;
+    char *command = NULL;
+    char *arguments[argc];
+
+    if (!sc)
+    {
+        VLOG_ERR("Structure pointer passed "
+                 "is null %s %d.\n",__FILE__,__LINE__);
+        return CMD_WARNING;
+    }
+
+    cmd_len = strlen(sc->userName) + strlen(sc->hostName)
+              + strlen(sc->srcFile);
+    command = (char*)malloc ((cmd_len+3)*sizeof(char));
+
+    if (!command)
+    {
+        VLOG_ERR("Malloc returned null %s %d.",__FILE__,__LINE__);
+        return CMD_WARNING;
+    }
+
+    len += sprintf(command+len, "%s@", sc->userName);
+    if (sc->isInteractive)
+    {
+        /* SFTP client for interative mode. */
+        len += sprintf(command+len, "%s", sc->hostName);
+        argc = 1;
+    }
+    else
+    {
+        /* SFTP client for non-interactive mode. */
+        len += sprintf(command+len, "%s:", sc->hostName);
+        len += sprintf(command+len, "%s", sc->srcFile);
+
+        /* If no destination location is mentioned then
+         * DEFAULT_DST is the destination. */
+        if (sc->dstFile != "")
+        {
+            dest = sc->dstFile;
+        }
+        else
+        {
+            dest = DEFAULT_DST;
+        }
+        arguments[1] = dest;
+    }
+
+    arguments[0] = command;
+    execute_command("sftp", argc, (const char **)arguments);
+
+    free (command);
+    return ret;
+}
+
 /* SFTP server enable config. */
 DEFUN ( cli_sftp_server_enable,
         cli_sftp_server_enable_cmd,
@@ -161,6 +231,117 @@ DEFUN ( cli_show_sftp_server,
       return show_sftp_server();
 }
 
+/* SFTP client interactive copy. */
+DEFUN ( cli_sftp_interactive,
+        cli_sftp_interactive_cmd,
+        "copy sftp WORD (A.B.C.D | X:X::X:X | WORD)",
+        COPY_STR
+        SFTP_CLIENT_STR
+        USERNAME_STR
+        HOST_IPv4
+        HOST_IPv6
+        HOSTNAME_STR )
+{
+    sftpClient sclient;
+    memset(&sclient, 0, sizeof(sftpClient));
+
+    /* Validation of input params. */
+    if (strlen((char*)argv[0]) > MAX_USERNAME_LEN)
+    {
+        vty_out(vty, "Username should be less than %d "
+                     "characters%s.",MAX_USERNAME_LEN, VTY_NEWLINE);
+        return CMD_SUCCESS;
+    }
+    else
+    {
+        sclient.userName = (char*)argv[0];
+    }
+
+    if (isalpha((int) *argv[1]))
+    {
+        if (strlen((char*)argv[1]) < MAX_HOSTNAME_LEN)
+        {
+            sclient.hostName = (char*)argv[1];
+        }
+        else
+        {
+            vty_out(vty, "Hostname should be less than %d characters "
+                         "or invalid host IP%s.",MAX_HOSTNAME_LEN, VTY_NEWLINE);
+            return CMD_SUCCESS;
+        }
+    }
+    else
+    {
+        sclient.hostName = (char*)argv[1];
+    }
+
+    sclient.srcFile = "";
+    sclient.dstFile = "";
+    sclient.isInteractive = true;
+
+    return sftp_client_copy(&sclient);
+}
+
+/* SFTP client non-interactive copy. */
+DEFUN ( cli_sftp_non_interactive_copy,
+        cli_sftp_non_interactive_cmd,
+        "copy sftp WORD (A.B.C.D | X:X::X:X | WORD) WORD [WORD]",
+        COPY_STR
+        SFTP_CLIENT_STR
+        USERNAME_STR
+        HOST_IPv4
+        HOST_IPv6
+        HOSTNAME_STR
+        SRC_FILENAME
+        DST_FILENAME )
+{
+    sftpClient sclient;
+    memset(&sclient, 0, sizeof(sftpClient));
+
+    /* Validation of input params. */
+    if (strlen((char*)argv[0]) > MAX_USERNAME_LEN)
+    {
+        vty_out(vty, "Username should be less than %d "
+                     "characters%s.",MAX_USERNAME_LEN, VTY_NEWLINE);
+        return CMD_SUCCESS;
+    }
+    else
+    {
+        sclient.userName = (char*)argv[0];
+    }
+
+    if (isalpha((int) *argv[1]))
+    {
+        if (strlen((char*)argv[1]) < MAX_HOSTNAME_LEN)
+        {
+            sclient.hostName = (char*)argv[1];
+        }
+        else
+        {
+            vty_out(vty, "Hostname should be less than %d characters "
+                         "or invalid host IP%s.",MAX_HOSTNAME_LEN, VTY_NEWLINE);
+            return CMD_SUCCESS;
+        }
+    }
+    else
+    {
+        sclient.hostName = (char*)argv[1];
+    }
+
+    sclient.srcFile = (char*)argv[2];
+    if((strcmp((char*)argv[3], "") == 0))
+    {
+        sclient.dstFile = "";
+    }
+    else
+    {
+        sclient.dstFile = (char*)argv[3];
+    }
+    sclient.isInteractive = false;
+
+    return sftp_client_copy(&sclient);
+}
+
 /* Install SFTP VTY commands. */
 void
 sftp_vty_init (void)
@@ -168,4 +349,6 @@ sftp_vty_init (void)
     install_element(ENABLE_NODE, &cli_show_sftp_server_cmd);
     install_element(CONFIG_NODE, &cli_sftp_server_enable_cmd);
     install_element(CONFIG_NODE, &cli_sftp_server_disable_cmd);
+    install_element(ENABLE_NODE, &cli_sftp_non_interactive_cmd);
+    install_element(ENABLE_NODE, &cli_sftp_interactive_cmd);
 }
