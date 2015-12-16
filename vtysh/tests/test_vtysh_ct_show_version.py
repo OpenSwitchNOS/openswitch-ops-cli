967c4ecca5a69a3d1e611e0213f3afe904345c99
Kei Nohguchi <kei.nohguchi@hpe.com>
2015-12-15 21:52:11 -0800

    chg: dev: OVSDB oriented "show version"
    "show version" CLI retrieves the OS name and version info from the
    OVSDB, which is based on the OS agnostic /etc/os-release file.
    
    Currently CLI uses system.software_info.os_name + system.switch_version
    as the "show version" contents, as shown below:
    
    switch# sh version
    OpenSwitch 0.1.0 (Build: developer_image)
    
    Here is the switch's /etc/os-relase info, for your reference:
    
    bash-4.3# cat /etc/os-release
    ID=openswitch
    NAME=OpenSwitch
    VERSION=0.1.0 (basil)
    VERSION_ID=0.1.0
    PRETTY_NAME=OpenSwitch 0.1.0 (basil)
    HOME_URL=http://www.openswitch.net
    BUILD_ID=developer_image
    
    VERSION_ID and BUILD_ID will be used for the switch_version value.
    
    Change-Id: I1beaa0383ad88f9075ddac5e79d1e5675c384013
    Signed-off-by: Kei Nohguchi <kei.nohguchi@hpe.com>
    Depends-On: I886488fbd13c5c2271d8bdbfe416c291e8277a70
    Depends-On: I38c35eafd4ea959276e31173770179c3062f02f8
    Depends-On: I1c9960098b77e97793b86cf78d2dbc40b4ff700e

---
+++ vtysh/tests/test_vtysh_ct_show_version.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+
+# (c) Copyright 2015 Hewlett Packard Enterprise Development LP
+#
+# GNU Zebra is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any
+# later version.
+#
+# GNU Zebra is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Zebra; see the file COPYING.  If not, write to the Free
+# Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+from mininet.node import Host
+from mininet.net import Mininet
+from mininet.topo import SingleSwitchTopo
+from opsvsi.opsvsitest import OpsVsiTest, OpsVsiLink, VsiOpenSwitch
+
+
+class ShowVersionTest(OpsVsiTest):
+    def setupNet(self):
+        # if you override this function, make sure to
+        # either pass getNodeOpts() into hopts/sopts
+        # of the topology that you build or into
+        # addHost/addSwitch calls
+        host_opts = self.getHostOpts()
+        switch_opts = self.getSwitchOpts()
+        system_topo = SingleSwitchTopo(k=0, hopts=host_opts,
+                                       sopts=switch_opts)
+        self.net = Mininet(system_topo, switch=VsiOpenSwitch,
+                           host=Host, link=OpsVsiLink,
+                           controller=None, build=True)
+        self.s1 = self.net.switches[0]
+
+    def setup(self):
+        self.old_switch_version = None
+        self.old_os_name = None
+        out = self.s1.ovscmd('ovs-vsctl list system')
+        lines = out.split('\n')
+        for line in lines:
+            if '_uuid' in line:
+                self.uuid = line.split(':')[1].strip()
+            elif 'switch_version' in line:
+                self.old_switch_version = line.split(':')[1].strip()
+            elif 'software_info' in line:
+                kvs = line.split(':')[1].strip(' {}\r\n')
+                if kvs:
+                    for kv in kvs.split(', '):
+                        key, value = kv.split('=')
+                        if 'os_name':
+                            self.old_os_name = value
+
+    def teardown(self):
+        self.__set_version(self.old_switch_version)
+        self.__set_os_name(self.old_os_name)
+
+    def __set_os_name(self, os_name):
+        if os_name:
+            self.s1.ovscmd('ovs-vsctl set system {0} '
+                           'software_info:os_name="{1}"'.
+                           format(self.uuid, os_name))
+        else:
+            self.s1.ovscmd('ovs-vsctl remove system {0} '
+                           'software_info os_name'.format(self.uuid))
+
+    def __set_version(self, version):
+        self.s1.ovscmd('ovs-vsctl -- set system {0} switch_version="{1}"'.
+                       format(self.uuid, version))
+
+    def __get_show_version_result(self):
+        return self.s1.cmdCLI('show version').split('\n')[1]
+
+    def check_show_version(self, os_name="OpenSwitch", version="0.1.0"):
+        self.__set_os_name(os_name)
+        self.__set_version(version)
+        assert os_name + " " + version == self.__get_show_version_result()
+
+
+class TestRunner:
+    @classmethod
+    def setup_class(cls):
+        cls.test = ShowVersionTest()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.test.stopNet()
+        cls.test = None
+
+    def setup(self):
+        self.test.setup()
+
+    def teardown(self):
+        self.test.teardown()
+
+    def test_os_name(self):
+        return self.test.check_show_version(os_name="TestOS")
+
+    def test_os_name_with_whitespace(self):
+        return self.test.check_show_version(os_name="TestVenter TestOS")
+
+    def test_normal_version(self):
+        return self.test.check_show_version(version="1.1.1")
+
+    def test_string_version(self):
+        return self.test.check_show_version(version="string_ver")
