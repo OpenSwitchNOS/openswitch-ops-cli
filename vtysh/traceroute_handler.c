dc4dbb0c7e0b6565777f964fa79d574130c9d813
Sudhakar <sudhakar.athikari@hpe.com>
2016-01-11 21:38:18 -0800

    Defect ID :511, Proper error message is displayed when traceroute is executed by admin user
    Change-Id: I3ab522c147701ccc8071d80dcf8088a8e2635ea1
    Signed-off-by: Sudhakar <sudhakar.athikari@hpe.com>

--- vtysh/traceroute_handler.c
+++ vtysh/traceroute_handler.c
@@ -55,8 +55,34 @@ bool traceroute_handler(tracerouteEntry *p, void (*fPtr)(char *buff))
         return false;
     }
 
-    /* Executing the command in the "swns" namespace, as the \
-    interfaces visible in vtysh are from "swns" */
+    /*
+     * As of now, traceroute is executed from swns namespace.
+     * swns namespace is accessible only by root users.
+     * Fix is added to display proper error message for other users.
+     * This is a temporary fix, fix will be removed once RBAC is implemented.
+     */
+
+    fp = popen("whoami", "r");
+
+    if (fp)
+    {
+        fgets(output, BUFSIZ, fp);
+        pclose(fp);
+        fp = NULL;
+
+        /* Removing new line at the end of output */
+        output[strlen(output)-1] = '\0';
+        if (strcmp(output, "root") != 0)
+        {
+            (*fPtr)("Traceroute is supported only for root users.");
+            return true;
+        }
+    }
+
+    /*
+     * Executing the command in the "swns" namespace, as the
+     * interfaces visible in vtysh are from "swns".
+     */
     len += sprintf(target+len, "%s ", SWNS_EXEC);
 
     /* Append default cmd either traceroute4 or traceroute6 */
