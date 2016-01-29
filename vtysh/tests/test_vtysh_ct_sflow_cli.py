2439871f108ceef3322abe841c96611d795a6114
Rohan Shah <rohan.shah@hpe.com>
2016-01-29 14:04:32 -0800

    chg: usr: removing the extra keywords from the sflow agent interface cli @minor
    Change-Id: If10cf88e92c799b7604c8dff068ee8d5b22aa5ef
    Signed-off-by: Rohan Shah <rohan.shah@hpe.com>

--- vtysh/tests/test_vtysh_ct_sflow_cli.py
+++ vtysh/tests/test_vtysh_ct_sflow_cli.py
@@ -180,11 +180,6 @@ class sflowConfigTest(OpsVsiTest):
         ret = s1.cmdCLI("sflow agent-interface 100")
         assert 'Invalid interface' in ret, 'Interface check not successful'
 
-        info("### Verfiying check for L3 interface ###\n")
-        ret = s1.cmdCLI("sflow agent-interface 19")
-        assert 'Interface 19 is not L3' in ret, \
-                'L3 interface check not successful'
-
         info("### Verifying correct setting of L3 agent interface ###\n")
         s1.cmdCLI("interface 19")
         s1.cmdCLI("ip address 10.10.10.10/32")
@@ -201,7 +196,7 @@ class sflowConfigTest(OpsVsiTest):
         s1.cmdCLI("interface 29")
         s1.cmdCLI("ip address 20.20.20.20/32")
         s1.cmdCLI("exit")
-        s1.cmdCLI("sflow agent-interface 29 agent-address-family ipv4")
+        s1.cmdCLI("sflow agent-interface 29 ipv4")
         ret = s1.cmdCLI("do show sflow")
         out = ret.split('\n')
         word_index = out.index('sFlow Configuration ')
@@ -229,7 +224,7 @@ class sflowConfigTest(OpsVsiTest):
 
         s1.cmdCLI("sflow enable")
         s1.cmdCLI("sflow sampling 54321")
-        s1.cmdCLI("sflow agent-interface 19 agent-address-family ipv6")
+        s1.cmdCLI("sflow agent-interface 19 ipv6")
         ret = s1.cmdCLI("do show running-config")
         out = ret.split('\n')
         word_index = out.index('sflow enable')
@@ -237,7 +232,7 @@ class sflowConfigTest(OpsVsiTest):
                 out[word_index+1] and \
                 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff port 65535 vrf ' \
                 'vrf_default' in out[word_index+2] and \
-                'sflow agent-interface 19 agent-address-family ipv6' in \
+                'sflow agent-interface 19 ipv6' in \
                 out[word_index+3] and 'sflow sampling 54321' in \
                 out[word_index+4], 'show running-config failure'
         info("### 'show running-config' verification successful###\n\n\n")
