f1c6a19ba610442747524c43b06690e872e55e05
Sanjay Dwivedi <sanjay.kum.dwivedi@hpe.com>
2016-02-09 03:24:51 -0800

    check added for subinterface if it is already created.
    Change-Id: I2be7285eba35ba5026abe3492ab303e3192192a1
    Signed-off-by: Sanjay Dwivedi <sanjay.kum.dwivedi@hpe.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -1559,6 +1559,42 @@ ALIAS (vtysh_exit_tftp_server,
       "quit",
       "Exit current mode and down to previous mode\n")
 #endif
+DEFUN (vtysh_interface,
+      vtysh_interface_cmd,
+      "interface IFNAME",
+      "Select an interface to configure\n"
+      "Interface's name\n")
+{
+  static char ifnumber[MAX_IFNAME_LENGTH];
+
+  if (strchr(argv[0], '.'))
+  {
+     return create_sub_interface(argv[0]);
+  }
+  else
+     vty->node = INTERFACE_NODE;
+
+  if (VERIFY_VLAN_IFNAME(argv[0]) == 0) {
+  vty->node = VLAN_INTERFACE_NODE;
+      GET_VLANIF(ifnumber, argv[0]);
+      if (create_vlan_interface(ifnumber) == CMD_OVSDB_FAILURE) {
+          return CMD_OVSDB_FAILURE;
+      }
+  }
+  else if (strlen(argv[0]) < MAX_IFNAME_LENGTH)
+  {
+    strncpy(ifnumber, argv[0], MAX_IFNAME_LENGTH);
+    default_port_add(ifnumber);
+  }
+  else
+  {
+    return CMD_ERR_NO_MATCH;
+  }
+  VLOG_DBG("%s ifnumber = %s\n", __func__, ifnumber);
+  vty->index = ifnumber;
+  return CMD_SUCCESS;
+}
+
 DEFUN (vtysh_interface_vlan,
        vtysh_interface_vlan_cmd,
        "interface vlan VLANID",
