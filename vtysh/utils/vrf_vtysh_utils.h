d36060bf74d38765a0dd5f3970ee7bc069fcb588
Dinesh MC <dinesh.m-c@hpe.com>
2016-02-11 00:51:10 -0800

    chg: usr: Disabling to load liblacpd_cli.so for modularizing ops-cli
    Change-Id: I89701974a6d11922f055ddcf78a6ece335a6bfc5
    Signed-off-by: Dinesh MC <dinesh.m-c@hpe.com>

---
+++ vtysh/utils/vrf_vtysh_utils.h
@@ -0,0 +1,24 @@
+/* LACP CLI utils commands header file
+ *
+ * Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * File: vrf_vtysh_utils.h
+ *
+ * Purpose: This file contains comman macro for lacp CLI.
+ */
+
+const struct ovsrec_vrf* port_vrf_lookup(const struct ovsrec_port *port_row);
