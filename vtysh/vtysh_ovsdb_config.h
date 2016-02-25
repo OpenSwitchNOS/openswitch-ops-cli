d8c73ab3f4c964c0bb99c6b19ea2b7d7a0a73039
Miles Cote <miles.cote@hpe.com>
2016-02-24 19:04:13 -0800

    chg: dev: Add qos trust cli
    Change-Id: Id72cd0ca4bb8738d245a02de0c035065f9e168d3
    Signed-off-by: Miles Cote <miles.cote@hpe.com>

--- vtysh/vtysh_ovsdb_config.h
+++ vtysh/vtysh_ovsdb_config.h
@@ -67,6 +67,7 @@ typedef enum vtysh_config_context_client_idenum
   e_vtysh_config_context_staticroute,
   e_vtysh_config_context_ecmp,
   e_vtysh_config_context_ntp,
+  e_vtysh_config_context_qos_trust,
   e_vtysh_config_context_client_id_max
 } vtysh_config_context_clientid;
 
