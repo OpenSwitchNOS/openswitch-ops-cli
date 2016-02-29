2d651446f45591632027a6fc718adcb5f55fc164
krishna <elluru.kri.mohan@hpe.com>
2016-02-28 19:46:56 -0800

    chg: dev: Fix incorrect retrieval of Default VRF record in ops-cli@minor     Currently many modules use ovsrec_vrf_first as default_vrf     record, Which eventually changes when new VRFs get added to     OVSDB. Since OVSDB uses hash table mechanism for storing     records, first record need not be first inserted record,     hence a library has been added for VRF record retrieval     which need to be used by all modules, for correct record     retrieval. Addresssing those changes in cli code.
    Change-Id: Ie2e3c3a3f1f6d376b8975092f5fbd31774339217
    Signed-off-by: krishna <elluru.kri.mohan@hpe.com>

--- vtysh/vrf_vty.h
+++ vtysh/vrf_vty.h
@@ -39,8 +39,6 @@ const struct ovsrec_port* port_check_and_add(const char *port_name, bool create,
 
 const struct ovsrec_vrf* port_vrf_lookup(const struct ovsrec_port *port_row);
 
-const struct ovsrec_vrf* vrf_lookup(const char *vrf_name);
-
 static int vrf_proxy_arp_toggle_state(const char *if_name, const char* value);
 
 #endif /* _VRF_VTY_H */
