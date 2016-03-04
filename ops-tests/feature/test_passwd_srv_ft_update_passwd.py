ec4e786a144b6eb2d64ba8f4c896538847655604
Mohamed Kahn <mohamed.kahn@hpe.com>
2016-03-03 16:39:19 -0800

    chg: dev: ops-cli: password server integration
    - removed passwd call from ops-cli
    - remove useradd/userdel call from ops-cli
    - changes for ops-cli to use ops-passwd-srv
    
    Change-Id: Ia0585c6048156f09fe80d86a71548c1ad01feb00
    Signed-off-by: Namsoo Kim <nkim@hpe.com>

---
+++ ops-tests/feature/test_passwd_srv_ft_update_passwd.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+OpenSwitch Feature Test Cases for the password server
+"""
+
+from pytest import mark
+
+TOPOLOGY = """
+# +-------+
+# |       |
+# |ops1   |
+# |       |
+# +-------+
+
+# Nodes
+# [image="genericx86-64:latest" type=openswitch name="OpenSwitch 1"] ops1
+[type=openswitch name="OpenSwitch 1"] ops1
+# Links
+"""
+
+
+@mark.platform_incompatible(['ostl'])
+def test_change_user_password(topology):
+    """
+    Test that user logged-on via CLI can change own password using 'password'
+    CLI command.
+
+    Using bash shell from the switch
+    1. ssh to switch as a user netop
+    2. run password command
+    3. change current password to '1111'
+    4. exit from CLI
+    5. re-login with a new password
+    """
+    ops1 = topology.get('ops1')
+    conn = "ssh -o StrictHostKeyChecking=no netop@localhost"
+    old_password = "netop"
+    new_password = "1111"
+    matches = ['fingerprint', 'continue', 'yes', 'no']
+
+    assert ops1 is not None
+
+    # get bash shell instance
+    bash_shell = ops1.get_shell('bash')
+
+    # connect to vtysh as netop via @localhost
+    print("SSH to localhost as netop")
+    # assert bash_shell.send_command(conn, matches) is 0
+    assert bash_shell.send_command(conn, matches=['password:']) is 0
+    assert bash_shell.send_command(old_password, matches=['switch#']) is 0
+
+    # change password
+    matches = ['old password:']
+    print("Execute password command")
+    assert bash_shell.send_command("password", matches) is 0
+
+    # enter old password
+    matches = ['new password:']
+    print("Enter old password")
+    assert bash_shell.send_command(old_password, matches) is 0
+
+    # enter new password
+    print("Enter new password")
+    assert bash_shell.send_command(new_password, matches) is 0
+
+    # confirm new password
+    matches = ['executed successfully.']
+    print("Re-enter new password")
+    assert bash_shell.send_command(new_password, matches) is 0
+
+    print("Password updated for netop")
+
+    # repeat above process with a new password
+    # exit from the ssh
+    matches = ['Connection to localhost closed.']
+    assert bash_shell.send_command('exit', matches) is 0
+
+    # connect to vtysh as netop via @localhost'
+    matches = ['password:']
+    print("Re-connect to SSH session as netop using new password")
+    assert bash_shell.send_command(conn, matches) is 0
+    assert bash_shell.send_command(new_password, matches=['switch#']) is 0
+
+    print("Change the password back to default")
+
+    # change password
+    matches = ['old password:']
+    print("Execute password command")
+    assert bash_shell.send_command("password", matches) is 0
+
+    # enter old password
+    matches = ['new password:']
+    print("Enter old password")
+    assert bash_shell.send_command(new_password, matches) is 0
+
+    # enter new password
+    print("Enter new password")
+    assert bash_shell.send_command(old_password, matches) is 0
+
+    # confirm new password
+    matches = ['executed successfully.']
+    print("Re-enter new password")
+    assert bash_shell.send_command(old_password, matches) is 0
+
+    # exit from the ssh
+    matches = ['Connection to localhost closed.']
+    assert bash_shell.send_command('exit', matches) is 0
+
+    print("Test test_change_user_password PASSED")
+
+
+@mark.platform_incompatible(['ostl'])
+def test_invalid_old_password(topology):
+    """
+    Test that user logged-on via CLI can change own password using 'password'
+    CLI command and provide the wrong password.  The password server must
+    reject the password change request and send error status back to CLI.
+
+    Using bash shell from the switch
+    1. ssh to switch as a user netop
+    2. run password command
+    3. change current password as 'FFFF'
+    4. verify that error "Old password did not match." has occurred
+    4. exit from CLI
+    5. re-login with a new password
+    """
+
+    ops1 = topology.get('ops1')
+    conn = "ssh -o StrictHostKeyChecking=no netop@localhost"
+    new_password = "1111"
+    wrong_password = "FFFF"
+    old_password = "netop"
+    matches = ['fingerprint', 'continue', 'yes', 'no']
+
+    assert ops1 is not None
+
+    # get bash shell instance
+    bash_shell = ops1.get_shell('bash')
+
+    # connect to vtysh as netop via @localhost
+    print("SSH to localhost as netop")
+    assert bash_shell.send_command(conn, matches=['password:']) is 0
+    assert bash_shell.send_command(old_password, matches=['switch#']) is 0
+
+    # change password
+    matches = ['old password:']
+    print("Execute password command")
+    assert bash_shell.send_command("password", matches) is 0
+
+    # enter old password
+    matches = ['new password:']
+    print("Enter invalid password")
+    assert bash_shell.send_command(wrong_password, matches) is 0
+
+    # enter new password
+    print("Enter new password")
+    assert bash_shell.send_command(new_password, matches) is 0
+
+    # confirm new password
+    matches = ['did not match']
+    print("Re-enter new password")
+    assert bash_shell.send_command(new_password, matches) is 0
+
+    # exit from the ssh
+    matches = ['Connection to localhost closed.']
+    assert bash_shell.send_command('exit', matches) is 0
+    print("Test test_invalid_old_password PASSED")
