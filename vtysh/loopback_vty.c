f97a10ad6ac8883e975c4cc70fa5e15fb627a939
Sanjay Dwivedi <sanjay.kum.dwivedi@hpe.com>
2016-04-28 04:28:40 -0700

    chg: fix: Bug:#887 Loopback(L3) : sh interface loopback brief is reflecting unnecessary inputs
    Change-Id: Ifab614324391a20bc7e99581653695f356eb1888
    Signed-off-by: Sanjay Dwivedi <sanjay.kum.dwivedi@hpe.com>

--- vtysh/loopback_vty.c
+++ vtysh/loopback_vty.c
@@ -681,6 +681,7 @@ DEFUN (cli_intf_show_interface_loopback_if,
         "Select a loopback interface\n")
 {
     const struct ovsrec_interface *ifrow = NULL;
+    const struct ovsrec_port *row = NULL;
     const char *cur_state = NULL;
     struct shash sorted_interfaces;
     const struct shash_node **nodes;
@@ -704,13 +705,12 @@ DEFUN (cli_intf_show_interface_loopback_if,
         /* Display the brief information. */
         vty_out(vty, "%s", VTY_NEWLINE);
         vty_out(vty, "---------------------------------------------------"
-                     "-----------------------------%s", VTY_NEWLINE);
-        vty_out(vty, "Ethernet      VLAN       Type       Mode     Status  "
-                     "    Reason       Speed    Port%s", VTY_NEWLINE);
-        vty_out(vty, "Interface                                          "
-                     "                 (Mb/s)   Ch# %s", VTY_NEWLINE);
-        vty_out(vty, "----------------------------------------------------"
-                     "----------------------------%s", VTY_NEWLINE);
+                             "%s", VTY_NEWLINE);
+        vty_out(vty, "Loop      IPv4 Address       Status  "
+                     "%s", VTY_NEWLINE);
+        vty_out(vty, "Interface %s", VTY_NEWLINE);
+        vty_out(vty, "---------------------------------------------------"
+                     "%s", VTY_NEWLINE);
     }
 
     vty_out (vty, "%s", VTY_NEWLINE);
@@ -736,15 +736,17 @@ DEFUN (cli_intf_show_interface_loopback_if,
         for (idx = 0; idx < count; idx++)
         {
             ifrow = (const struct ovsrec_interface *)nodes[idx]->data;
+            OVSREC_PORT_FOR_EACH(row, idl)
+            {
+                if (strcmp(row->name, ifrow->name) == 0)
+                   break;
+            }
             /* Display brief information. */
             vty_out (vty, "%-12s", ifrow->name);
-            vty_out (vty, "--    "); /*VLAN */
-            vty_out (vty, "    loopback  "); /*type */
-            vty_out (vty, "   routed "); /*mode - routed or not*/
+            vty_out (vty, "%-12s", (row && row->ip4_address) ?
+                     row->ip4_address : "--");
 
             vty_out (vty, "      up "); /*Admin status*/
-            vty_out (vty, "      auto");
-            vty_out (vty, "      -- ");  /*Port channel*/
             vty_out (vty, "%s", VTY_NEWLINE);
         }
     }
