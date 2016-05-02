c6ef950e327d88405f4c3ee8072f8759a1dea260
Prerana B.H <prerana.bh@hpe.com>
2016-05-01 21:15:44 -0700

    fix: dev: Bug #842: Compiler warning in VRF module CLI files
    In previous fix for this bug #842 had a memory leak.
    
    Removing smap_remove function for both features
    local_proxy_arp and Proxy_arp in vrf_no_routing
    function. Since there is already function
    ovsrec_port_delete which handles removing port_row.
    
    Change-Id: I453403ce0a710e17688a57b85359ee295694e4b2
    Signed-off-by: Prerana B.H <prerana.bh@hpe.com>

--- vtysh/vrf_vty.c
+++ vtysh/vrf_vty.c
@@ -784,7 +784,6 @@ vrf_no_routing (const char *if_name)
   int64_t* tag = NULL;
   int tag_count = 0;
   size_t i;
-  struct smap smap_other_config;
 
   status_txn = cli_do_config_start ();
 
@@ -870,18 +869,6 @@ vrf_no_routing (const char *if_name)
                 }
             }
         }
-
-      /* Cache the current state of 'other-config' column for this port record */
-      smap_clone(&smap_other_config, &port_row->other_config);
-
-      /* Disable proxy-arp on the port */
-      smap_remove(&smap_other_config,
-              PORT_OTHER_CONFIG_MAP_PROXY_ARP_ENABLED);
-
-     /* Disable local-proxy-arp on the port */
-     smap_remove(&smap_other_config,
-                 PORT_OTHER_CONFIG_MAP_LOCAL_PROXY_ARP_ENABLED);
-
      vrf_ports = xmalloc (sizeof *vrf_row->ports * (vrf_row->n_ports - 1));
       for (i = n = 0; i < vrf_row->n_ports; i++)
         {
