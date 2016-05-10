9b8352a1d6c9ca5ec6225c2b4cf07f60bb26f2e4
Vinay K <vinay.k@hpe.com>
2016-05-09 23:57:40 -0700

    chg: dev: Fix for CR 2145 - Restricting the SFTP client operation only to admin.
    Change-Id: Iad3059f2035f15a6da6e9b3256746a5e937d2bae
    Signed-off-by: Vinay K <vinay.k@hpe.com>

--- vtysh/sftp_vty.c
+++ vtysh/sftp_vty.c
@@ -23,12 +23,42 @@
  */
 #include "sftp_vty.h"
 
+static bool isValidUser (void);
 static int show_sftp_server (void);
 static int sftp_server_enable_disable (bool);
 static int sftp_client_copy (sftpClient *sc);
 
 extern struct ovsdb_idl *idl;
 VLOG_DEFINE_THIS_MODULE(vtysh_sftp_cli);
+
+/*-----------------------------------------------------------------------------
+| Function : isValidUser
+| Responsibility : To validate the user logged in device and to verify
+|                  if the user has the permissions.
+| Return : true, if the user has the permissions
+|          false, if the user do not have the required permissions
+-----------------------------------------------------------------------------*/
+static bool
+isValidUser (void)
+{
+    struct passwd *pw = NULL;
+
+    pw = getpwuid( getuid());
+    if (pw == NULL)
+    {
+        VLOG_ERR("ERROR :Unknown User.\n");
+        vty_out(vty, "ERROR :Unknown User%s", VTY_NEWLINE);
+        return false;
+    }
+
+    /* The only allowed valid user is an admin user */
+    if (!(rbac_check_user_permission(pw->pw_name, RBAC_SYS_MGMT)))
+    {
+        return false;
+    }
+
+    return true;
+}
 
 /*-----------------------------------------------------------------------------
 | Function : show_sftp_server
@@ -245,6 +275,16 @@ DEFUN ( cli_sftp_interactive,
     sftpClient sclient;
     memset(&sclient, 0, sizeof(sftpClient));
 
+    /* Validate the permission of the user accessing the device */
+    if (!isValidUser())
+    {
+        VLOG_ERR("ERROR :This user has no authorisation to "
+                 "execute this command\n");
+        vty_out(vty, "ERROR :This user has no authorisation to "
+                     "execute this command%s", VTY_NEWLINE);
+        return CMD_SUCCESS;
+    }
+
     /* Validation of input params. */
     if (strlen((char*)argv[0]) > MAX_USERNAME_LEN)
     {
@@ -297,6 +337,16 @@ DEFUN ( cli_sftp_non_interactive_copy,
 {
     sftpClient sclient;
     memset(&sclient, 0, sizeof(sftpClient));
+
+    /* Validate the permission of the user accessing the device */
+    if (!isValidUser())
+    {
+        VLOG_ERR("ERROR :This user has no authorisation to "
+                 "execute this command\n");
+        vty_out(vty, "ERROR :This user has no authorisation to "
+                     "execute this command%s", VTY_NEWLINE);
+        return CMD_SUCCESS;
+    }
 
     /* Validation of input params. */
     if (strlen((char*)argv[0]) > MAX_USERNAME_LEN)
