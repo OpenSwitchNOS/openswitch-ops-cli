3288de6cb55b217df2d0378b02fbabdd4674d6e9
Kunal Shridhar <kunal.shridhar@hpe.com>
2016-05-24 14:39:23 -0700

    Fix broken help strings in router-bgp mode
    Tags - fix, dev
    
    TG - 1172
    
    Change-Id: I8ee0a4b775b5b2589af01759481858bec54bd6f8
    Signed-off-by: Kunal Shridhar <kunal.shridhar@hpe.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -1111,7 +1111,7 @@ DEFUNSH (VTYSH_BGPD,
       "address-family vpnv4 unicast",
       "Enter Address Family command mode\n"
       "Address family\n"
-      "Address Family Modifier\n")
+      "Address family modifier\n")
 {
    vty->node = BGP_VPNV4_NODE;
    return CMD_SUCCESS;
@@ -1123,7 +1123,7 @@ DEFUNSH (VTYSH_BGPD,
       "address-family ipv4 unicast",
       "Enter Address Family command mode\n"
       "Address family\n"
-      "Address Family Modifier\n")
+      "Address family modifier\n")
 {
    vty->node = BGP_IPV4_NODE;
    return CMD_SUCCESS;
@@ -1135,7 +1135,7 @@ DEFUNSH (VTYSH_BGPD,
       "address-family ipv4 multicast",
       "Enter Address Family command mode\n"
       "Address family\n"
-      "Address Family Modifier\n")
+      "Address family modifier\n")
 {
    vty->node = BGP_IPV4M_NODE;
    return CMD_SUCCESS;
@@ -1158,7 +1158,7 @@ DEFUNSH (VTYSH_BGPD,
       "address-family ipv6 unicast",
       "Enter Address Family command mode\n"
       "Address family\n"
-      "Address Family Modifier\n")
+      "Address family modifier\n")
 {
    vty->node = BGP_IPV6_NODE;
    return CMD_SUCCESS;
@@ -1170,7 +1170,7 @@ DEFUNSH (VTYSH_BGPD,
       "address-family ipv6 multicast",
       "Enter Address Family command mode\n"
       "Address family\n"
-      "Address Family Modifier\n")
+      "Address family modifier\n")
 {
    vty->node = BGP_IPV6M_NODE;
    return CMD_SUCCESS;
