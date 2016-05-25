fb7917ed2e0e1f9d0cd19cbbbd54892a2fa0c548
Lei Wang <lei.wang3@hpe.com>
2016-05-25 13:36:43 -0700

    Remove CLI placeholder 'show bgp memory'
    bgp memory usage information displayed
    as part of "diag-dump bgp"
    
    Tags - new, dev
    TG - 239
    
    Change-Id: Ied3d54868c276c6c38a19eb7ee82b55119551213
    Signed-off-by: Lei Wang <lei.wang3@hpe.com>

--- vtysh/bgp_vty.c
+++ vtysh/bgp_vty.c
@@ -9213,17 +9213,6 @@ DEFUN(show_bgp_views,
     return CMD_SUCCESS;
 }
 
-DEFUN(show_bgp_memory,
-      show_bgp_memory_cmd,
-      "show bgp memory",
-      SHOW_STR
-      BGP_STR
-      "Global BGP memory statistics\n")
-{
-    report_unimplemented_command(vty, argc, argv);
-    return CMD_SUCCESS;
-}
-
 /* 'Show ip bgp summary' commands. */
 
 /*Funtion to get the statistics from neighbor table. */
@@ -11775,11 +11764,6 @@ bgp_vty_init(void)
     /* Ttl_security commands. */
     install_element(BGP_NODE, &neighbor_ttl_security_cmd);
     install_element(BGP_NODE, &no_neighbor_ttl_security_cmd);
-
-    /* "Show bgp memory" commands. */
-    install_element(VIEW_NODE, &show_bgp_memory_cmd);
-    install_element(RESTRICTED_NODE, &show_bgp_memory_cmd);
-    install_element(ENABLE_NODE, &show_bgp_memory_cmd);
 
     /* "Show bgp views" commands. */
     install_element(VIEW_NODE, &show_bgp_views_cmd);
