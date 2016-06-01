21d05c7788ec8a8781a29123a07eb40a36ad8d76
Vladimir Vargas <vladimir.vargas.cordero@hpe.com>
2016-06-01 09:07:47 -0600

    chg: dev: Add nodes for IPsec CLI
    Added nodes for IPsec Policy configurations, needed for
    IPsec CLI implementation.
    
    Change-Id: Iaa8d0c1721bac6f5d16c2ef0dbe0977c8d29c4dc
    Signed-off-by: Vladimir Vargas <vladimir.vargas.cordero@hpe.com>

--- lib/command.h
+++ lib/command.h
@@ -149,6 +149,8 @@ enum node_type
   VLAN_INTERFACE_NODE,          /* VLAN Interface Node*/
   SUB_INTERFACE_NODE,           /* Sub Interface mode node. */
   LOOPBACK_INTERFACE_NODE,      /* Loopback Interface mode node. */
+  IPSEC_IKE_NODE,                   /* IPsec policy node */
+  IPSEC_IKE_ISAKMP_NODE,            /* IPsec ISAKMP node */
 #endif
   VTY_NODE,			/* Vty node. */
 };
