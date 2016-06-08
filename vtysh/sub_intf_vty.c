4bc19d19cebd2b0c2eb7b814be1756903fd042f2
Kunal Shridhar <kunal.shridhar@hpe.com>
2016-06-08 14:17:27 -0700

    minor changes to show int {id} subinterface command
    TG - 1311
    
    Change-Id: Ia86d76c5da5dd15c29e75872a11c6d532d2eec53
    Signed-off-by: Kunal Shridhar <kunal.shridhar@hpe.com>

--- vtysh/sub_intf_vty.c
+++ vtysh/sub_intf_vty.c
@@ -985,64 +985,72 @@ DEFUN (cli_intf_show_subintferface_if_all,
         "Show brief info of interface\n")
 {
     const struct ovsrec_interface *ifrow = NULL;
-    char subif_prefix[MAX_IFNAME_LENGTH] = {0}, *subif_ptr = NULL;
+    char intf_name[MAX_IFNAME_LENGTH] = {0};
+    char *subif_prefix = NULL;
     struct shash sorted_interfaces;
     bool brief = false;
     const struct shash_node **nodes;
     int idx, count;
-    bool known_intf = false;
+    const char *parent_intf = NULL;
 
-    if ((argv[0] != NULL) && (strchr(argv[0], '.'))){
-         return CMD_ERR_NO_MATCH;
+    if (argv[0] != NULL) {
+        /* handle invalid arguments like vlan12, bridge_normal, 15.1 */
+        if (!atoi(argv[0]) || strchr(argv[0], '.')) {
+            return CMD_ERR_NO_MATCH;
+        }
+        parent_intf = argv[0];
     }
+
     if ((NULL != argv[1]) && (strcmp(argv[1], "brief") == 0))
     {
         brief = true;
     }
 
-    if (NULL != argv[0])
-    {
-        strcpy(subif_prefix, argv[0]);
-        subif_ptr = strtok(subif_prefix, ".");
-    }
-
     shash_init(&sorted_interfaces);
 
+    /* Only collect sub-interfaces of the parent interface specified
+     * by argv[0] */
     OVSREC_INTERFACE_FOR_EACH(ifrow, idl)
     {
-        shash_add(&sorted_interfaces, ifrow->name, (void *)ifrow);
-    }
-
-    nodes = sort_sub_interfaces(&sorted_interfaces);
-    count = shash_count(&sorted_interfaces);
-
-    for (idx = 0; idx < count; idx++)
-    {
-        ifrow = (const struct ovsrec_interface *)nodes[idx]->data;
-
         if (strcmp(ifrow->type, OVSREC_INTERFACE_TYPE_VLANSUBINT) != 0)
         {
             continue;
         }
-        if (strncmp(ifrow->name,subif_ptr, strlen(subif_ptr)) != 0)
+
+        /* extract the parent interface from sub-interface name */
+        strcpy(intf_name, ifrow->name);
+        subif_prefix = strtok(intf_name, ".");
+        if (strcmp(subif_prefix,parent_intf) != 0)
         {
             continue;
         }
-        if (!known_intf && brief)
-        {
-            display_subinterface_brief_header();
-        }
 
-        cli_show_subinterface_row(ifrow, brief);
-        known_intf = true;
+        shash_add(&sorted_interfaces, ifrow->name, (void *)ifrow);
     }
-    if (!known_intf && argv[0])
+
+
+    nodes = sort_sub_interfaces(&sorted_interfaces);
+    count = shash_count(&sorted_interfaces);
+
+    if (count <= 0) {
+        vty_out (vty, "No sub-interfaces configured for interface %s%s",
+                parent_intf, VTY_NEWLINE);
+        return CMD_ERR_NOTHING_TODO;
+    }
+
+    if (brief) {
+        display_subinterface_brief_header();
+    }
+
+    for (idx = 0; idx < count; idx++)
     {
-        vty_out(vty,"%% Unknown interface%s", VTY_NEWLINE);
+        ifrow = (const struct ovsrec_interface *)nodes[idx]->data;
+        cli_show_subinterface_row(ifrow, brief);
     }
 
     shash_destroy(&sorted_interfaces);
     free(nodes);
+
     return CMD_SUCCESS;
 }
 
