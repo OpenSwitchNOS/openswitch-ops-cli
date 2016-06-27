cf7873dc14514255aeace8b302573246b0acfd26
Ravi Jonnadula <rjonnadu@linkedin.com>
2016-06-27 12:24:58 -0700

    - CLI command support to configure BFD for bgp sessions.
    Change-Id: Ica7002280178ba395031ca56d1c2c795a1b3f21e
    Signed-off-by: Ravi Jonnadula <rjonnadu@linkedin.com>

--- vtysh/vtysh_ovsdb_if.c
+++ vtysh/vtysh_ovsdb_if.c
@@ -162,6 +162,8 @@ bgp_ovsdb_init()
     ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_remote_as);
     ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_allow_as_in);
     ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_local_as);
+    ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_bfd_fallover_enable);
+    ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_bfd_session);
     ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_weight);
     ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_tcp_port_number);
     ovsdb_idl_add_column(idl, &ovsrec_bgp_neighbor_col_advertisement_interval);
@@ -547,7 +549,6 @@ ovsdb_init(const char *db_path)
 
     /* VRF tables. */
     vrf_ovsdb_init();
-
 
     /* Policy tables. */
     policy_ovsdb_init();
