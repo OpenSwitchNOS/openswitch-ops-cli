630a2499e7a53d5f5b817675576c797e99c4ea3f
vkez <ezhilan.vk@hpe.com>
2016-06-27 21:28:45 -0700

    CLI misbehave after enable page break up option.
    Tags: fix, usr
    TG-1449
    
    Change-Id: I1def1d7401bd89390337357f2df70afbc0c3764f
    Signed-off-by: vkez <ezhilan.vk@hpe.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -96,6 +96,8 @@ int vtysh_show_startup = 0;
 
 static int passwd_srv_pubkey_len;
 
+extern int skip_further_execution;
+
 /* Struct VTY. */
 struct vty *vty;
 
@@ -609,6 +611,7 @@ vtysh_execute (const char *line)
    int retVal = CMD_SUCCESS;
    vty_interrupted_flag_set(0);
    reset_page_height();
+   skip_further_execution = 0;
    retVal = vtysh_execute_func (line, 1);
    vty_interrupted_flag_set(0);
    reset_page_height();
