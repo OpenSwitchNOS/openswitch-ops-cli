14f81f8aafe03156c1837927f7bea23a9ef0b081
Jayachandra Babu K <jayachandra.bab.k@hpe.com>
2016-07-06 20:25:36 -0700

    new: usr: CLI changes to add indexing to VRF table and "show vrf" display changes.
    This commit includes changes to use the OVSDB enhancements functionality to
    display VRFs created in sorted order, changes to sort the Interfaces list
    coreectly for a VRF.
    In the list of VRFs displayed, vrf_default is printed first and then the other VRFs.
    
    Change-Id: Ic395bf60e1053c6f2c2da5c89e4d49220140436e
    Signed-off-by: Jayachandra Babu K <jayachandra.bab.k@hpe.com>

--- vtysh/utils/vrf_vtysh_utils.c
+++ vtysh/utils/vrf_vtysh_utils.c
@@ -19,6 +19,8 @@
  * Responsibility : Definition of common vrf CLI function.
  */
 
+#include <ctype.h>
+
 #include "ovsdb-idl.h"
 #include "vswitch-idl.h"
 #include "vrf_vtysh_utils.h"
@@ -44,30 +46,258 @@ port_vrf_lookup (const struct ovsrec_port *port_row)
     return NULL;
 }
 
+/* Extract the interface type from interface name */
+static char *
+extract_intf_type (const char *intf_name)
+{
+    const char *p = intf_name;
+    int count = 0;
+
+    while (*p)
+    {
+        if (isdigit (*p))
+        {
+            break;
+        }
+        else
+        {
+            p ++;
+            count ++;
+        }
+    }
+    char *intf_type = (char *) malloc(sizeof (char) * (count + 1));
+    strncpy (intf_type, intf_name, count);
+    intf_type[count] = '\0';
+
+    return intf_type;
+}
+
+/* Extract the interface id from the interface-name */
+static unsigned long
+extract_tag_from_intf_name (const char *intf_name)
+{
+    unsigned long val = 0;
+    const char *p = intf_name;
+    while (*p)
+    {
+        if (isdigit(*p))
+        {
+            val = val*10 + (*p-'0');
+        }
+        p++;
+    }
+    return val;
+}
+
+static int
+compare_both_proper_intf_names (const char *name_intf1, const char *name_intf2)
+{
+    char *type_intf1 = extract_intf_type (name_intf1);
+    char *type_intf2 = extract_intf_type (name_intf2);
+
+    int str_comparison_result = strcmp (type_intf1, type_intf2);
+    free(type_intf1);
+    free(type_intf2);
+    if (str_comparison_result == 0)
+    {
+        unsigned long tag_intf1 = extract_tag_from_intf_name (name_intf1);
+        unsigned long tag_intf2 = extract_tag_from_intf_name (name_intf2);
+
+        if (tag_intf1 == tag_intf2)
+        {
+            return 0;
+        }
+        else if (tag_intf1 < tag_intf2)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else if (str_comparison_result < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+static int
+compare_both_intf_numbers (const char *name_intf1, const char *name_intf2)
+{
+    uint id_intf1 = 0, id_intf2 = 0;
+    uint ext_id_intf1 = 0, ext_id_intf2 = 0;
+    unsigned long tag_sub_intf1 = 0, tag_sub_intf2 = 0;
+
+    /* To  handle cases like 10, 54-2, 52-1.1 etc. */
+    sscanf(name_intf1, "%d-%d.%lu", &id_intf1, &ext_id_intf1, &tag_sub_intf1);
+    sscanf(name_intf2, "%d-%d.%lu", &id_intf2, &ext_id_intf2, &tag_sub_intf2);
+
+    /* To handle cases like 10.1, 54.2 etc. */
+    if (strchr (name_intf1, '.') && !strchr (name_intf1, '-'))
+    {
+        sscanf(name_intf1, "%d.%lu", &id_intf1, &tag_sub_intf1);
+    }
+
+    if (strchr (name_intf2, '.') && !strchr (name_intf2, '-'))
+    {
+        sscanf(name_intf2, "%d.%lu", &id_intf2, &tag_sub_intf2);
+    }
+
+    if(id_intf1 == id_intf2)
+    {
+        if(ext_id_intf1 == ext_id_intf2)
+        {
+            /* For proper positioning of subinterfaces */
+            if (tag_sub_intf1 == tag_sub_intf2)
+            {
+                return 0;
+            }
+            else if (tag_sub_intf1 < tag_sub_intf2)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else if(ext_id_intf1 < ext_id_intf2)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else if (id_intf1 < id_intf2)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+static int
+compare_digit_and_name (const char *name1, const char *name2)
+{
+    if (isdigit(*name1))
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int compare_both_numbers(const char *name1, const char *name2)
+{
+    uint i1 = 0, i2 = 0;
+
+    sscanf(name1, "%d", &i1);
+    sscanf(name2, "%d", &i2);
+    if (i1 == i2)
+    {
+        return 0;
+    }
+    else if (i1 < i2)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
 /*-----------------------------------------------------------------------------
 | Function       : compare_nodes_vrf
-| Responsibility : the function is used for comparing two VRF element
+| Responsibility : Custom comparator function used for comparing two VRF element
 | Parameters:
 |    *a_: Pointer to first VRF element
 |    *b_: Pointer to second VRF element
 | Return:
 |    int: zero if two VRF elements are equal, 1 if the first elemnt is
 |         bigger than second, -1 if the first element is smaller than second
+| sort comparator function.
+| Case 1: When both vrf names consists of numbers only
+| Case 2: When one vrf name consists of numbers and other is proper name
+| Case 3: When both vrf names are proper names with interface type and id
 -----------------------------------------------------------------------------*/
 int
-compare_nodes_vrf(void *a_, void *b_)
+compare_nodes_vrf (const void *a_, const void *b_)
 {
-    struct shash_node **a = a_;
-    struct shash_node **b = b_;
-    uint i1 = 0, i2 = 0;
+    const struct ovsrec_vrf *a = (struct ovsrec_vrf *)a_;
+    const struct ovsrec_vrf *b = (struct ovsrec_vrf *)b_;
+    char *name1 = (a)->name;
+    char *name2 = (b)->name;
 
-    sscanf(((*a)->name + VRF_VLAN_INTF_ID_OFFSET), "%d", &i1);
-    sscanf(((*b)->name + VRF_VLAN_INTF_ID_OFFSET), "%d", &i2);
-
-    if (i1 == i2)
-        return 0;
-    else if (i1 < i2)
-        return -1;
+    if (isdigit(*name1) && isdigit(*name2))
+    {
+         return compare_both_numbers (name1, name2);
+    }
+    /* Case 2 */
+    else if (isdigit(*name1) || isdigit(*name2))
+    {
+        return compare_digit_and_name (name1, name2);
+    }
+    /* Case 3 */
     else
-        return 1;
+    {
+        return strcmp(name1, name2);
+    }
+
+    return 0;
+}
+
+/*-----------------------------------------------------------------------------
+| Function       : compare_interface_nodes_vrf
+| Responsibility : the function is used for comparing two interface elements
+| Parameters:
+|    *a_: Pointer to first interface element
+|    *b_: Pointer to second interface element
+| Return:
+|    int: zero if two elements are equal, 1 if the first elemnt is
+|         bigger than second, -1 if the first element is smaller than second
+| sort comparator function.
+| Case 1: When both interface name consists of numbers only
+|         Example: 10 and 12-1 etc.
+| Case 2: When one interface name consists of numbers and other is proper name
+|         Example: 10 and vlan20 etc.
+| Case 3: When both interface names are proper names with interface type and id
+|         Example: vlan20 and lo23 etc.
+-----------------------------------------------------------------------------*/
+int
+compare_interface_nodes_vrf (const void *a_, const void *b_)
+{
+    const struct shash_node *const *a = a_;
+    const struct shash_node *const *b = b_;
+    char *name_intf1 = (*a)->name;
+    char *name_intf2 = (*b)->name;
+
+    /* Case 1 */
+    if (isdigit(*name_intf1) && isdigit(*name_intf2))
+    {
+         return compare_both_intf_numbers (name_intf1, name_intf2);
+    }
+    /* Case 2 */
+    else if (isdigit(*name_intf1) || isdigit(*name_intf2))
+    {
+        return compare_digit_and_name (name_intf1, name_intf2);
+    }
+    /* Case 3 */
+    else
+    {
+        return compare_both_proper_intf_names (name_intf1, name_intf2);
+    }
+
+    return 0;
 }
