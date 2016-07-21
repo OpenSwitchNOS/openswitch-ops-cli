11a1a9c474b54be7a3e9773e07fd7275a6592010
Sachin Gupta <sachin.gupta3@hpe.com>
2016-07-20 23:03:12 -0700

    Description: Toronto: Erase startup config command should get confirmation from the user.
    Tags: dev
    
    Change-Id: I8744996025a78943ad4bdd841d0686efe8c9edc0
    Signed-off-by: Sachin Gupta <sachin.gupta3@hpe.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -2547,10 +2547,30 @@ DEFUN (vtysh_erase_startupconfig,
        ERASE_STR
        "Contents of startup configuration\n")
 {
-    char *arguments[] = {"delete", "startup-config"};
-    execute_command ("cfgdbutil", 2, (const char **)arguments);
+    char flag = '0';
+    vty_out(vty,"\rStartup configuration will be deleted.\nDo you want to continue [y/n]?");
+    while(1)
+    {
+        flag=getchar();
+        if (flag == 'y')
+        {
+            char *arguments[] = {"delete", "startup-config"};
+            execute_command ("cfgdbutil", 2, (const char **)arguments);
+            break;
+        }
+        else if (flag == 'n')
+        {
+            break;
+        }
+        else
+        {
+            vty_out(vty,"\r                              ");
+            vty_out(vty,"\rDo you want to continue [y/n]?");
+        }
+    }
     return CMD_SUCCESS;
 }
+
 #ifndef ENABLE_OVSDB
 DEFUN (vtysh_ping,
       vtysh_ping_cmd,
