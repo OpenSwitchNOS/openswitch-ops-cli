673549e1985901fec2b8cad8e479021fb95ccf6c
Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>
2016-08-17 11:54:51 -0700

    Seggregation of CONFIG NODES
    Change-Id: I36d2624f2f8c70d4cf4fbc9c6b62187107516691
    Signed-off-by: Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>

--- lib/command.h
+++ lib/command.h
@@ -100,58 +100,53 @@ enum node_type
   AUTH_NODE,			/* Authentication mode of vty interface. */
   RESTRICTED_NODE,		/* Restricted view mode */
   VIEW_NODE,			/* View node. Default mode of vty interface. */
+
   AUTH_ENABLE_NODE,		/* Authentication mode for change enable. */
   ENABLE_NODE,			/* Enable node. */
+
   CONFIG_NODE,			/* Config node. Default mode of config file. */
-  SERVICE_NODE, 		/* Service node. */
-  DEBUG_NODE,			/* Debug node. */
+
   AAA_NODE,			/* AAA node. */
-  KEYCHAIN_NODE,		/* Key-chain node. */
-  KEYCHAIN_KEY_NODE,		/* Key-chain key node. */
+
   INTERFACE_NODE,		/* Interface mode node. */
-  ZEBRA_NODE,			/* zebra connection node. */
-  TABLE_NODE,			/* rtm_table selection node. */
+
   RIP_NODE,			/* RIP protocol mode node. */
   RIPNG_NODE,			/* RIPng protocol mode node. */
-  BABEL_NODE,			/* Babel protocol mode node. */
+
   BGP_NODE,			/* BGP protocol mode which includes BGP4+ */
   BGP_VPNV4_NODE,		/* BGP MPLS-VPN PE exchange. */
   BGP_IPV4_NODE,		/* BGP IPv4 unicast address family.  */
   BGP_IPV4M_NODE,		/* BGP IPv4 multicast address family.  */
   BGP_IPV6_NODE,		/* BGP IPv6 address family */
   BGP_IPV6M_NODE,		/* BGP IPv6 multicast address family. */
+
   OSPF_NODE,			/* OSPF protocol mode */
   OSPF6_NODE,			/* OSPF protocol for IPv6 mode */
-  ISIS_NODE,			/* ISIS protocol mode */
-  PIM_NODE,			/* PIM protocol mode */
-  MASC_NODE,			/* MASC for multicast.  */
-  IRDP_NODE,			/* ICMP Router Discovery Protocol mode. */
-  IP_NODE,			/* Static ip route node. */
-  PREFIX_NODE,			/* Prefix list node. */
-  PREFIX_IPV6_NODE,		/* Prefix list node. */
-  AS_LIST_NODE,			/* AS list node. */
-  COMMUNITY_LIST_NODE,		/* Community list node. */
+
   RMAP_NODE,			/* Route map node. */
-  SMUX_NODE,			/* SNMP configuration node. */
-  DUMP_NODE,			/* Packet dump node. */
-  FORWARDING_NODE,		/* IP forwarding node. */
-  PROTOCOL_NODE,                /* protocol filtering node */
+
 #ifdef ENABLE_OVSDB
   DHCP_SERVER_NODE,             /* DHCP server node */
+
   TFTP_SERVER_NODE,             /* TFTP server node */
+
   ACCESS_LIST_NODE,             /* Access Control List node */
+
   MIRROR_NODE,                  /* Mirror node */
+
   VLAN_NODE,                    /* Vlan Node */
+
   MGMT_INTERFACE_NODE,          /* Management Interface Node*/
+
   LINK_AGGREGATION_NODE,        /* Link aggregation Node*/
+
   QOS_QUEUE_PROFILE_NODE,       /* QoS Queue Profile Node. */
   QOS_SCHEDULE_PROFILE_NODE,    /* QoS Schedule Profile Node. */
+
   VLAN_INTERFACE_NODE,          /* VLAN Interface Node*/
   SUB_INTERFACE_NODE,           /* Sub Interface mode node. */
   LOOPBACK_INTERFACE_NODE,      /* Loopback Interface mode node. */
-  IPSEC_IKE_NODE,                   /* IPsec policy node */
-  IPSEC_IKE_ISAKMP_NODE,            /* IPsec ISAKMP node */
-  VRRP_IF_NODE,                /* VRRP interface node */
+
 #endif
   VTY_NODE,			/* Vty node. */
 };
