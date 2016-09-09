0b43c9b14bf82a9196a6c811b835b3d6b131ae5a
Akshar Ranka <akshar.ranka@hpe.com>
2016-09-09 15:01:20 -0700

    Changes to incorporate running config for vxlan interface
    Change-Id: I286605b28a8a4eeffbf8390fd9124998390f4c6b
    Signed-off-by: Akshar Ranka <akshar.ranka@hpe.com>

--- vtysh/vtysh_ovsdb_config.h
+++ vtysh/vtysh_ovsdb_config.h
@@ -80,6 +80,7 @@ typedef enum vtysh_config_context_client_idenum
   e_vtysh_config_context_ecmp,
   e_vtysh_config_context_ntp,
   e_vtysh_config_context_tunnel,
+  e_vtysh_config_context_tunnel_intf,
   e_vtysh_config_context_mstp,
   e_vtysh_config_context_syslog,
   e_vtysh_config_context_snmp,
